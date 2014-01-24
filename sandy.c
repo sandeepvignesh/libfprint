5e307cbae1f1de97353175591a45275355a3cd2c
Sandeep A G <sarcot@jaguarlandrover.com.com>
2014-01-24 01:09:05 -0800

    as

---
+++ sandy.c
@@ -0,0 +1,17 @@
+  /* 12L05.c: Stopping at the null character */
+  #include <stdio.h>
+
+  main()
+  {
+     double list_data[6] = {
+         1.12345,
+         2.12345,
+         3.12345,
+         4.12345,
+         5.12345};
+    //int i;
+    /*  array_ch[i] in logical test */
+    //for (i=0; array_ch[i] != '\0'; i++)
+       printf("%d", sizeof(list_data));
+    return 0;
+ }
